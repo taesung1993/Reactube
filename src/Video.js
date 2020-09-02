3fe312e8fbe810bcaa3f49f51e663d5c30a3e690
taesung1993 <cheonyulin@gmail.com>
2020-09-02 20:07:12 +0900

    add route function on react-router-dom

--- src/Video.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Video = ({ title }) => {
-  return <h1>{title}</h1>;
-};
-
-export default Video;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDom from "react-dom";
 import App from "./App";
 
-// Component는 HTML을 반환하는 함수
-// React 와 HTML의 조합을 JSX라고 부름
-// jsx는 리액트에만 있음
-
-ReactDOM.render(<App />, document.getElementById("main"));
+ReactDom.render(<App />, document.getElementById("main"));
